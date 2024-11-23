625cb705c9bb51a2efb28bbdaa4a49eadc2e115a
abithach <158759929+abithach@users.noreply.github.com>
2024-11-23 15:30:34 +0530

    Inherience code

---
+++ Inheritance.java
@@ -0,0 +1,26 @@
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+
+class Puppy extends Dog {
+    void weep() {
+        System.out.println("The puppy weeps.");
+    }
+}
+
+public class Inheritance{
+    public static void main(String[] args) {
+        Puppy puppy = new Puppy();
+        puppy.eat();
+        puppy.bark();
+        puppy.weep();
+    }
+}
